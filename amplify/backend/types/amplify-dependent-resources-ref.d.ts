03f5d06062d5d61d6f6e015736a3381fab113a14
James Gowen <theandthem@gmail.com>
2023-06-02 13:51:29 +0100

    simple notes app created

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "jpa": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "jpafe63ac24": {
       "AppClientID": "string",
